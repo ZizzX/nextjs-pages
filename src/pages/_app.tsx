dcc454d3116019e005e56e6acfa6b50cb4fa998d
Isapov Aziz <a.isapov@globerce.capital>
2024-07-14 16:38:16 +0500

    добавил правила стилей eslint, stylelint
    Кастомные стили

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import "@/styles/globals.css";
+import React from "react";
 import type { AppProps } from "next/app";
-import { Noto_Sans } from 'next/font/google'
+import { Noto_Sans } from 'next/font/google';
+import "@/styles/globals.css";
 
 const notoSans = Noto_Sans({
   weight: ['300', '400', '500', '600', '700'],
@@ -8,9 +9,9 @@ const notoSans = Noto_Sans({
   subsets: ['latin'],
   display: 'swap',
   variable: '--font-noto-sans',
-})
+});
 
-export default function App({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps): React.JSX.Element {
   return (
       <main className={notoSans.className}>
         <Component {...pageProps} />
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,4 @@
-import  {Html, Head, Main, NextScript, DocumentContext} from "next/document";
+import  { Html, Head, Main, NextScript } from "next/document";
 import React from "react";
 
 export default function Document(): React.JSX.Element {
