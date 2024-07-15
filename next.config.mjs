527c083965d61e5600db8c17abfb74d14b8d8ea9
Isapov Aziz <a.isapov@globerce.capital>
2024-07-15 12:26:48 +0500

    Добавил svgr, настройка, конфиг, типы для работы с SVG иконками

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,33 @@
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    const fileLoaderRule = config.module.rules.find((rule) =>
+        rule.test?.test?.('.svg'),
+    );
+
+    config.module.rules.push(
+        {
+            ...fileLoaderRule,
+            test: /\.svg$/i,
+            resourceQuery: /url/,
+        },
+        {
+            test: /\.svg$/i,
+            issuer: fileLoaderRule.issuer,
+            resourceQuery: { not: [...fileLoaderRule.resourceQuery.not, /url/] },
+            use: ['@svgr/webpack'],
+        },
+    );
+
+    fileLoaderRule.exclude = /\.svg$/i;
+
+    return config;
+  },
 };
 
 export default nextConfig;
